bf4006ae4e356c32abc5321d02085571bb7a3650
rcshim04 <rcshim04@gmail.com>
2021-05-22 19:25:38 -0400

    Update Blog and Nav

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,10 @@
-//let imagediv = document.getElementById("images")
-//let textdiv = document.getElementById("text")
-function getPosts() {
-    fetch('https://campagne-api.waba359.repl.co/posts.json')
-        .then(response => response.json())
-        .then(data => console.log(data));
+async function getPosts() {
+    let json = await (await fetch('https://campagne-api.waba359.repl.co/posts.json')).json()
+}
+async function setPost(id) {
+    let json = await (await fetch('https://campagne-api.waba359.repl.co/posts/'+id+'.json')).json()
+    console.log(json)
+    let imagediv = document.getElementById("images")
+    let textdiv = document.getElementById("text")
+    
 }
