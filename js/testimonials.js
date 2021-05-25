2f56eac4177eae50a57eecda32bd14f1f2b2a53a
rcshim04 <rcshim04@gmail.com>
2021-05-24 20:57:45 -0400

    Added testimonials

---
+++ js/testimonials.js
@@ -0,0 +1,36 @@
+async function getTestimonials(filter) {
+    let testifys = await (await fetch('https://campagne-api.waba359.repl.co/testimonials.json')).json();
+    let testifylist = document.getElementById("testimonials");
+    testifylist.innerHTML = "";
+    testifys.testimonials.sort((a, b) => parseInt(b.pts, 10) - parseInt(a.pts, 10));
+    for(var testimonial of testifys.testimonials) {
+        if(filter == "" || filter == testimonial.grade) {
+            let hyperlink = document.createElement("a");
+            let testimonialwrapper = document.createElement("div");
+            let thumbnail = document.createElement("img");
+            let text = document.createElement("div");
+            let name = document.createElement("h3");
+            let subtitle = document.createElement("p");
+            let test = document.createElement("p");
+            testimonialwrapper.classList.add("testimonial");
+            thumbnail.src = "https://campagne-api.waba359.repl.co/images/"+testimonial.thumbnail;
+            thumbnail.alt = testimonial.name+" Profile Photo";
+            thumbnail.classList.add("img");
+            text.classList.add("text");
+            hyperlink.href = testimonial.link;
+            hyperlink.target = "blank"
+            name.innerHTML = testimonial.name;
+            subtitle.innerHTML = "Grade "+testimonial.grade + (testimonial.roles == "" ? "" : " | "+testimonial.roles);
+            test.innerHTML = testimonial.test;
+            hyperlink.appendChild(name);
+            text.appendChild((testimonial.link == "" ? name : hyperlink));
+            text.appendChild(subtitle);
+            text.appendChild(document.createElement("hr"));
+            text.appendChild(test);
+            testimonialwrapper.appendChild(thumbnail);
+            testimonialwrapper.appendChild(text);
+            testifylist.appendChild(testimonialwrapper);
+        }
+    }
+}
+getTestimonials("");
