dc7b8d12f2a72c1dacf9b20f2a5141d608850bb0
rcshim04 <rcshim04@gmail.com>
2021-05-27 01:12:52 -0400

    Added new event

--- js/calendar.js
+++ js/calendar.js
@@ -12,14 +12,20 @@ document.addEventListener("DOMContentLoaded", function() {
             {
                 title: "Arts in Bloom",
                 start: "2021-05-25",
-                end: "2021-05-28",
-                color: "let(--status3)"
+                end: "2021-05-29",
+                color: "var(--status3)"
             },
             {
                 title: "Greenpeace Biodiversity",
                 start: "2021-05-25",
                 end: "2021-05-25",
-                color: "let(--status0)"
+                color: "var(--status0)"
+            },
+            {
+                title: "SAC Elections",
+                start: "2021-06-02",
+                end: "2021-06-04",
+                color: "var(--status1)"
             }
         ]
     });
